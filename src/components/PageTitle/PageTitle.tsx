6dd757ff4f5839e6747da1301ef609babcbe7b36
Vitor Machado <vitor.p.mach@gmail.com>
2021-05-02 03:26:16 -0300

    Top margin for page title

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -3,7 +3,7 @@ import './PageTitle.scss';
 
 export default function PageTitle(/* props: IPageTitleProp */) {
   return (
-    <div className="d-inline-block text-light">
+    <div className="d-inline-block text-light mt-4">
       <h1 className="text-right mb-0">Top 100 Albums</h1>
       <p className="text-right font-weight-bold">from iTunes</p>
     </div>
