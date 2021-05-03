20b939ba4a2b4530a6c99a89095db549e936f0a3
Vitor Machado <vitor.p.mach@gmail.com>
2021-05-03 00:24:19 -0300

    Author below title, minor card size change

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -4,7 +4,15 @@ export default function PageTitle() {
   return (
     <div className="d-inline-block text-light mt-4">
       <h1 className="text-right mb-0">Top 100 Albums</h1>
-      <p className="text-right font-weight-bold">from iTunes</p>
+      <p className="text-right font-weight-bold mb-0">from iTunes</p>
+      <p className="text-right text-muted font-weight-light mb-0">
+        <small>
+          Developed by{' '}
+          <a className="text-muted" href="https://www.vitormach.dev/" target="_blank" rel="noopener noreferrer">
+            Vitor Machado
+          </a>
+        </small>
+      </p>
     </div>
   );
 }
